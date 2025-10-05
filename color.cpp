a49fffdc7ec4cc534283fdbc45b8588e67e6ff22
Borcay <dlitoalv@gmail.com>
2025-10-05 17:57:48 -0500

    Primer commit

---
+++ color.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    system("color 0A");
+    //     ?  ?
+    //     |  +-- Texto amarillo claro
+    //     +---- Fondo azul
+
+    printf("Ventana con fondo azul y texto amarillo claro!\n");
+    return 0;
+}
+
